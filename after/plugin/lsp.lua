e98de3589e0ed16c5a280871db6b41d4ab2b6fe3
thoaif <thoaif.ahmed@gmail.com>
2023-12-10 13:35:12 +0500

    Added lsp support for bash

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,10 +1,11 @@
 local lsp = require('lsp-zero')
+local lsp_config = require('lspconfig')
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
   -- Replace the language servers listed here
   -- with the ones you want to install
-  ensure_installed = { 'rust_analyzer', 'lua_ls' },
+  ensure_installed = { 'rust_analyzer', 'lua_ls', 'bashls' },
 })
 
 
@@ -45,11 +46,14 @@ lsp.on_attach(function(client, bufnr)
 end)
 
 
+-- bash
+lsp_config.bashls.setup({})
+
 -- lua config
-require'lspconfig'.lua_ls.setup {
+lsp_config.lua_ls.setup {
   on_init = function(client)
     local path = client.workspace_folders[1].name
-    if not vim.loop.fs_stat(path..'/.luarc.json') and not vim.loop.fs_stat(path..'/.luarc.jsonc') then
+    if not vim.loop.fs_stat(path .. '/.luarc.json') and not vim.loop.fs_stat(path .. '/.luarc.jsonc') then
       client.config.settings = vim.tbl_deep_extend('force', client.config.settings, {
         Lua = {
           runtime = {
