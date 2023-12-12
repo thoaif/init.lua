b0748a0a7c34ae1e59a4e711594eb7da0f3d0e37
thoaif <thoaif@lottiefiles.com>
2023-12-12 14:56:56 +0500

    updated neotest config

--- after/plugin/neotest.lua
+++ after/plugin/neotest.lua
@@ -29,4 +29,21 @@ require('neotest').setup({
 })
 
 
-vim.api.nvim_set_keymap("n", "<leader>tf", "<cmd>lua require('neotest').run.run(vim.fn.expand('%'))<cr>", {})
+
+-- run all tests in the file
+vim.keymap.set("n", "<leader>tf", function() require('neotest').run.run(vim.fn.expand('%')) end, {})
+
+-- run nearest test
+vim.keymap.set("n", "<leader>tt", function() require('neotest').run.run() end, {})
+
+-- debug nearest test
+vim.keymap.set("n", "<leader>td", function() require('neotest').run.run({ strategy = "dap" }) end, {})
+
+-- debug the current file
+vim.keymap.set("n", "<leader>tF", function() require('neotest').run.run({ vim.fn.expand('%'), strategy = "dap" }) end, {})
+
+-- attach to the neareast test
+vim.keymap.set("n", "<leader>ta", function() require('neotest').run.attach() end, {})
+
+-- toggle test summary
+vim.keymap.set("n", "<leader>ts", function() require('neotest').summary.toggle() end, {})
