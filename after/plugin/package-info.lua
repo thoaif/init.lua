99eccfdf901d1f19e330aacb4e5d5a62c969bb14
thoaif <thoaif.ahmed@gmail.com>
2023-12-17 14:45:50 +0500

    removed commands for package-info

--- after/plugin/package-info.lua
+++ after/plugin/package-info.lua
@@ -1,17 +1,4 @@
 require("package-info").setup({
   package_manager = "pnpm",
+  hide_up_to_date = true,
 })
-
--- Define the command 'PackageInfo' to show dependency versions
-vim.api.nvim_create_user_command(
-  'PackageInfo',
-  require('package-info').show,
-  {}
-)
-
--- Define the command 'PackageInfo hide' to hide dependency versions
-vim.api.nvim_create_user_command(
-  'PackageInfoHide',
-  require('package-info').hide,
-  {}
-)
